c7479a000efdcb6d160a7d0cf53ac1f3adad2db3
Gabrielė <67112338+xonism@users.noreply.github.com>
2021-04-01 20:43:52 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,74 +5,91 @@ const calculator = document.getElementById('calculator').rows;
 
 const screen = calculator[0].cells[0];
 
-const deleteButton = calculator[1].cells[0];
-const divideButton = calculator[1].cells[1];
-const multiplyButton = calculator[1].cells[2];
-const backButton = calculator[1].cells[3];
+const buttons$ = {
+    delete: calculator[1].cells[0],
+    divide: calculator[1].cells[1],
+    multiply: calculator[1].cells[2],
+    back: calculator[1].cells[3],
 
-const sevenButton = calculator[2].cells[0];
-const eightButton = calculator[2].cells[1];
-const nineButton = calculator[2].cells[2];
-const minusButton = calculator[2].cells[3];
+    seven: calculator[2].cells[0],
+    eight: calculator[2].cells[1],
+    nine: calculator[2].cells[2],
+    minus: calculator[2].cells[3],
 
-const fourButton = calculator[3].cells[0];
-const fiveButton = calculator[3].cells[1];
-const sixButton = calculator[3].cells[2];
-const plusButton = calculator[3].cells[3];
+    four: calculator[3].cells[0],
+    five: calculator[3].cells[1],
+    six: calculator[3].cells[2],
+    plus: calculator[3].cells[3],
 
-const oneButton = calculator[4].cells[0];
-const twoButton = calculator[4].cells[1];
-const threeButton = calculator[4].cells[2];
-const equalsButton = calculator[4].cells[3];
+    one: calculator[4].cells[0],
+    two: calculator[4].cells[1],
+    three: calculator[4].cells[2],
+    equals: calculator[4].cells[3],
 
-const zeroButton = calculator[5].cells[0];
-const dotButton = calculator[5].cells[1];
+    zero: calculator[5].cells[0],
+    dot: calculator[5].cells[1],
+};
 
-const buttonArray = [ divideButton, multiplyButton, sevenButton, eightButton, nineButton, minusButton, fourButton, fiveButton, sixButton, plusButton, oneButton, twoButton, threeButton, zeroButton, dotButton ];
+const buttonArray = [ buttons$.divide, buttons$.multiply, buttons$.seven, buttons$.eight, buttons$.nine, buttons$.minus, buttons$.four, buttons$.five, buttons$.six, buttons$.plus, buttons$.one, buttons$.two, buttons$.three, buttons$.zero, buttons$.dot ];
 
-const addListenerForNumbers = (element) => {
+const isScreenCapacityExceeded = () => screen.innerHTML.length >= allowedNumberOfSymbols;
+const isScreenDisplayingError = () => screen.innerHTML === errorMessage;
+const isUserTryingToEnterTwoSymbolsInARow = ( element ) => isNaN( screen.innerHTML.slice(-1) ) === isNaN( element.innerHTML );
+
+const isScreenZero = () => screen.innerHTML == 0 && screen.innerHTML.length == 1; // "0." also evaluates true in first condition
+
+const addListenerForNumbers = ( element ) => {
     element.addEventListener( "click", function() {
-        if ( screen.innerHTML.length >= allowedNumberOfSymbols || screen.innerHTML === errorMessage ) return;
-
-        if ( screen.innerHTML == 0 && screen.innerHTML.length == 1 ) { // "0." also evaluates true in first condition
-            screen.innerHTML = element.innerHTML;
-        } else {
-            screen.innerHTML += element.innerHTML;
-        }
+        if ( isScreenCapacityExceeded() || isScreenDisplayingError() ) return;
+        isScreenZero() ? screen.innerHTML = element.innerHTML : screen.innerHTML += element.innerHTML;
     } )
 };
 
-const addListenerForSymbols = (element) => {
-    element.addEventListener( "click", function() {
-        if ( screen.innerHTML.length < allowedNumberOfSymbols && isNaN( screen.innerHTML.slice(-1) ) != isNaN(element.innerHTML) && screen.innerHTML != errorMessage ) {
-            screen.innerHTML += element.innerHTML;
-        }
+const addListenerForSymbols = ( symbol ) => {
+    symbol.addEventListener( "click", function() {
+        if ( isScreenCapacityExceeded() || isScreenDisplayingError() || isUserTryingToEnterTwoSymbolsInARow( symbol ) ) return;
+        screen.innerHTML += symbol.innerHTML;
     } )
 };
 
-deleteButton.addEventListener( "click", function() {
-    screen.innerHTML = 0;
-} )
+const addListenerForDelete = () => {
+    buttons$.delete.addEventListener( "click", function() {
+        screen.innerHTML = 0;
+    } )
+};
 
-backButton.addEventListener( "click", function() { // deleting the last number should result in zero, not blank cell
-    if ( screen.innerHTML != errorMessage ) {
+const addListenerForBack = () => {
+    buttons$.back.addEventListener( "click", function() { // deleting the last number should result in zero, not blank cell
+        if ( isScreenDisplayingError() ) return;
         screen.innerHTML = screen.innerHTML.length === 1 ? 0 : screen.innerHTML.slice(0, -1);
+    } )
+};
+
+const addListenerForEquals = () => {
+    buttons$.equals.addEventListener( "click", function() {
+        if ( isNaN( screen.innerHTML.slice(-1) ) ) return;
+        
+        const screenEval = eval( screen.innerHTML );
+        const numberOfSymbolsOnScreenAfterEval = screenEval.toString().length;
+    
+        if ( isFinite( screenEval ) && numberOfSymbolsOnScreenAfterEval <= allowedNumberOfSymbols ) {
+            screen.innerHTML = screenEval;
+        } else if ( isFinite( screenEval ) && numberOfSymbolsOnScreenAfterEval > allowedNumberOfSymbols ) {
+            screen.innerHTML = parseFloat( screenEval ).toFixed(10);
+        } else {
+            screen.innerHTML = errorMessage;
+        }
+    }  )
+};
+
+function init () {
+    for ( let i = 0; i < buttonArray.length; i++ ) {
+        isNaN( buttonArray[i].innerHTML ) ? addListenerForSymbols( buttonArray[i] ) : addListenerForNumbers( buttonArray[i] );
     }
-} )
 
-equalsButton.addEventListener( "click", function() {
-    const screenEval = eval(screen.innerHTML);
-    const symbolsOnScreenAfterEval = screenEval.toString().length;
-
-    if ( isFinite( screenEval ) && symbolsOnScreenAfterEval <= allowedNumberOfSymbols ) {
-        screen.innerHTML = screenEval;
-    } else if ( isFinite( screenEval ) && symbolsOnScreenAfterEval > allowedNumberOfSymbols ) {
-        screen.innerHTML = parseFloat( screenEval ).toFixed(10);
-    } else {
-        screen.innerHTML = errorMessage;
-    }
-}  )
-
-for ( let i = 0; i < buttonArray.length; i++ ) {
-    isNaN( buttonArray[i].innerHTML ) ? addListenerForSymbols( buttonArray[i] ) : addListenerForNumbers( buttonArray[i] );
+    addListenerForDelete();
+    addListenerForBack();
+    addListenerForEquals();
 }
+
+init();
